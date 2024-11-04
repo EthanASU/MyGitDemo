b59755ed815aea23658320a3a9e78c1c930c9fa0
Ethan <ejriver2@asu.edu>
2024-11-03 21:53:26 -0700

    Update welcome message in 'welcome.java'

--- welcome.java
+++ welcome.java
@@ -1,5 +1,5 @@
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Welcome to My Git Demo Project!");
+        System.out.println("Welcome to the Git tutorial!");
     }
 }
