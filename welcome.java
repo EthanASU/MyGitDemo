511557b3c1093fcb26afac64a02c48ec8e7c9106
Ethan <ejriver2@asu.edu>
2024-11-03 21:46:16 -0700

    Updata welcome message on main branch

---
+++ welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to My Git Demo Project!");
+	System.out.println("Hello from the main branch!");
+    }
+}
